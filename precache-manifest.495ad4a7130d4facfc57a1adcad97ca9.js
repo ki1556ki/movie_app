71361b59ed678e3e42aa124a8c559af28c148dbe
taeyeon <ki1556ki@naver.com>
2019-06-26 13:57:41 +0900

    Updates

---
+++ precache-manifest.495ad4a7130d4facfc57a1adcad97ca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e163df8b095a0888e2995463c504c1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7dc803205676788805a1",
+    "url": "/movie_app/static/css/main.2c61d6a0.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "7dc803205676788805a1",
+    "url": "/movie_app/static/js/main.4395fb89.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
